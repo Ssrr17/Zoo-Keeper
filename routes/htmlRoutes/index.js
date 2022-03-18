a21dc508bfd7db869db173dd0409dc6c7e5f09d5
Salieu Sarr <95061054+Ssrr17@users.noreply.github.com>
2022-03-18 00:45:16 -0400

    last commit

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,18 +1,20 @@
-const path = require('path');
-const router = require('express').Router();
+const path = require("path");
+const router = require("express").Router();
 
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-  });
-  router.get('/animals', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/animals.html'));
-  });
-  router.get('/zookeepers', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
-  });
-  //The * will act as a wildcard, meaning any route that wasn't previously defined will fall under this request
-  router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-  });
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../public/index.html"));
+});
 
-  module.exports = router;
+router.get("/animals", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../public/animals.html"));
+});
+
+router.get("/zookeepers", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../public/zookeepers.html"));
+});
+
+router.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+module.exports = router;
